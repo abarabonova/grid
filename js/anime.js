5a9dc3831785f22dd51809fd7f79397419964aaf
abarabonova <55283014+abarabonova@users.noreply.github.com>
2020-05-07 14:07:36 +0300

    готово

--- js/anime.js
+++ js/anime.js
@@ -1,4 +1,8 @@
 function view(n) {
     style = document.getElementById(n).style;
     style.display = (style.display == 'block') ? 'none' : 'block';
+
+    if (style.display == 'block') {
+      style.color == 'chartreuse'
+    }
 }
